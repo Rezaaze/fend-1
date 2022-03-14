8cc00383151596aa2e9a12ea86be8985ae6f0579
Rezaaze <khani.ali54@googlemail.com>
2022-03-14 18:26:50 +0100

    first commit

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,49 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
+const projectData = {"entrys":[]};
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors')
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+
+const port = 8000;
+
+const server = app.listen(port,listening);
+
+function listening (){
+    console.log("server is running")
+    console.log(`your server is running on port : ${port}`)
+}
+
+app.get("/get", sendData);
+
+function sendData(req,res){
+    res.send(projectData)
+    console.log("Datasend")
+}
+
+app.post("/post", recieveData);
+
+function recieveData(req, res){
+    
+    
+    projectData.entrys.push(req.body)
+    if(projectData.entrys.length > 10){
+        projectData.entrys.shift();
+    } 
+    console.log(projectData)
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,115 @@
 /* Global Variables */
-
+const api_key = "b076b3276229daa59919e693823fcecb";
+const api  = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
+const api_loc = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip},DE&appid=${api_key}`
+// created an empty object for holding variables for the backend
+const data = {}
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// created 2 variables and saved the button element and the content in them
+const button = document.getElementById('generate');
+
+const content =document.getElementById('date');
+console.log(content)
+
+// added eventlistener to the button 
+button.addEventListener("click", (e)=>{
+    e.preventDefault();
+    // saved the value from the textarea and input from zipcode in variables
+    const feelings = document.getElementById('feelings');
+    const zip = document.getElementById('zip');
+
+    data.date =newDate;
+    data.user_response = feelings.value;
+    // first api request for getting the location from zipcode pls only use german zipcodes like 60437 for frankfurt 
+    api_for_location(api_key,zip.value)
+    .then(res=>{
+        api_request_for_weather(api_key,res)
+        .then(res=>{
+            data.temperature = res.main.temp.toFixed(1);
+            postData(data)
+            getData()
+            .then(res=>update_content(res))
+            .catch(err=>("error in click event:", err))
+        })
+    })
+    //clearing the form inputs for new request
+    zip.value = null;
+    
+    feelings.value = null; 
+    
+    
+    
+
+    
+        
+    
+})
+
+const SERVER = "http://localhost:8000"
+
+//function for post data to server
+function postData(data){
+   return fetch(`${SERVER}/post`,{
+        method:'POST',
+        mode: "cors",
+        headers:{
+            'Content-Type':'application/json',
+            "Access-Control-Allow-Origin": SERVER,
+        },
+        body:JSON.stringify(data)
+    })
+    .then(()=>console.log("success"))
+    .catch(err=>console.log("Error in postData", err))
+}
+//function to get data from server
+function getData(){
+   return fetch(`${SERVER}/get`,{
+        method:'GET',
+        mode: "cors",
+        headers:{
+            'Content-Type':'application/json',
+            "Access-Control-Allow-Origin": SERVER,
+        }
+    })
+    .then(res=>res.json())
+    .catch(err=>console.log("Error in getData", err))
+}
+//api request for weather
+function api_request_for_weather (api_key,city){
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&appid=${api_key}&units=metric`)
+        .then(res=>res.json())
+        .catch(err=>console.log("error in request weather:",err))
+}
+//api request for location
+function api_for_location (api_key,zip){
+    return fetch(`http://api.openweathermap.org/geo/1.0/zip?zip=${zip},DE&appid=${api_key}`)
+            .then(res=>res.json())
+            .catch(err=>console.log("error in api_loc:", err))
+
+}
+// function for updateing UI with data from server
+function update_content(data){
+    
+    content.innerText="";
+    const ente =  data.entrys.map((item) =>{
+            const LI = document.createElement('LI');
+            LI.innerText = `Date : ${item.date} // Comment : ${item.user_response} // Temperatur : ${item.temperature} Celsius`;
+            return LI  
+
+    })
+
+    
+    for(let i = ente.length-1; i>=0 ;i--){
+        
+        content.appendChild(ente[i]);
+        
+        
+    }
+}
+// updating UI everytime site is refreshed
+getData()
+.then(res=>update_content(res))
+.catch(err=>("error in click event:", err))
